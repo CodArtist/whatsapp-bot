caddcaae271d10d6a4d66f91f501b8c559f47901
Harsh Jain <72666944+CodArtist@users.noreply.github.com>
2023-10-06 22:53:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const client = new Client({
+    puppeteer:{
+        headless:true,
+        args:[
+            "-no-sandbox",
+            "-disable-setuid-sandbox",
+            "-disable-dev-shm-usage"
+        ]
+    }
+
+
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
